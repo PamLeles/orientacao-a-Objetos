9d58ea32084f96ca211844e15f94f81aeb69b35e
PamLeles <pamela_leles_sousa@outlook.com>
2022-03-14 22:31:02 -0300

    classe ContaBancaria

---
+++ README.md
@@ -0,0 +1,6 @@
+# Orientação a Objetos
+## Atividade: Conta Bancária
+
+Testando os conceitos de Orientação a Objetos simulando a criação de diversos tipos de contas bancárias e operações disponíveis em cada uma.
+
+   
---
+++ contabank.js
@@ -0,0 +1,30 @@
+class ContaBancaria{
+    construtor(agencia, numero, tipo, saldo){
+        this.agencia = agencia;
+        this.numero = numero;
+        this.tipo = tipo;
+        this._saldo = saldo;  
+    }
+
+    get saldo(){
+        return this._saldo;
+    }
+    set saldo(valor){
+        this._saldo = valor;
+    }
+    sacar(valor){
+        if(valor . this._saldo){
+            return 'Operação negada';
+        }
+        this._saldo = this._saldo - valor;
+        return this._saldo;
+    }
+    depositar(valor){
+        this._saldo = this._saldo + valor;
+
+        return this._saldo;
+    }
+
+
+    }
+
